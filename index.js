076f277e4de44cfe5fe3a842fd45ab2a9daa833c
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-03-12 14:26:06 -0700

    Added more weather data

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
+function refreshWeather(response){
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+let city = response.data.city ;
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date= new Date();
+
+
+cityElement.innerHTML= (city);
+timeElement.innerHTML= formatDate(date);
+descriptionElement.innerHTML=(response.data.condition.description);
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+temperatureElement.innerHTML= Math.round(temperature);
+
+}
+
+function formatDate(date){
+    
+    let minutes= date.getMinutes();
+    let hours = date.getHours();
+    let days =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday", "Sunday"];
+    let day= days[date.getDay()];
+
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+return `${day} ${hours}:${minutes}`;
+}
+
+
+function searchCity(city) {
+    let apiKey = "8e4a0bf13d97t36283b48a970944023o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchsubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity (searchInput.value);
 }
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchsubmit);
+
+searchCity("Nigeria");
+
+
+
