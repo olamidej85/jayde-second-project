c0756d78d9795caefcca5b36e1b0785cb1e67ab2
olamidej85 <161619447+olamidej85@users.noreply.github.com>
2024-03-22 16:02:21 -0700

    Added Forecast api functions

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML= Math.round(temperature);
 
+getForecast(response.data.city);
 }
 
 function formatDate(date){
@@ -49,31 +50,49 @@ function handleSearchsubmit(event){
     searchCity (searchInput.value);
 }
 
-function displayForecast() {
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+return days [date.getDay()];
+}
+
+
+function getForecast(city){
+    let apiKey = "8e4a0bf13d97t36283b48a970944023o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=mertric`;
+    axios(apiUrl).then(displayForecast);
+
+}
+
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
     
-    let days = ["Tue", "Wed", "Thur", "Fri", "Sar"];
     let forecastHTML = "";
 
-    days.forEach (function(day){
+    response.data.daily.forEach (function(day, index){
+if (index < 5){
+
 forecastHTML= forecastHTML + `
 <div class="weather-forecast-day">
-     <div class="weather-forecast-date">${day}</div>
-     <div class=" weather-forecast-icon">🌤️</div>
+     <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src= "${day.condition.icon_url}" class="weather-forecast-icon"/>
      <div class="weather-forecast-temperatures">
         <span class="weather-forecast-temperature-max">
-        <strong>18°</strong>
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
         </span>
-        <span class="weather-forecast-temperature-min">12°</span>  
+        <span class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}°</span>  
  </div>
 </div>`;
+}
     } );
+
     forecastElement.innerHTML = forecastHTML;
+
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchsubmit);
 
 searchCity("Nigeria");
-displayForecast()
 
